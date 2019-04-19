b071b678fb3ccf3780378f91e9d8a59b0e28541d
fachiolc <fachiolc@gmail.com>
2019-04-18 23:18:51 -0400

    LogoutServlet Works

--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -25,8 +25,7 @@ public class LoginServlet extends HttpServlet {
 		// TODO Auto-generated constructor stub
 	}
 	// @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// Step 1: set content type
 		response.setContentType("text/html");
 		// Step 2: get the printwriter
@@ -36,22 +35,19 @@ public class LoginServlet extends HttpServlet {
 		String userName = request.getParameter("userName");
 		String password1 = request.getParameter("password1");
 		System.out.println("chk1");
-		if (userName == null || userName.equals(""))
-		{
+		if (userName == null || userName.equals("")){
 			out.print("Username missing");
 			RequestDispatcher rd = request.getRequestDispatcher("index.html");
 			rd.forward(request,response);
 			System.out.println("chk2a");
 		}
-		else if (password1 == null || password1.equals(""))
-		{
+		else if (password1 == null || password1.equals("")) {
 			out.print("Password missing");
 			RequestDispatcher rd = request.getRequestDispatcher("index.html");
 			rd.include(request,response);
 			System.out.println("chk2b");
 		}
-		else 
-		{
+		else {
 			System.out.println("employee page linked");
 			if (userType.equals("Employee")){
 				System.out.println("Employee logging in");
@@ -59,7 +55,7 @@ public class LoginServlet extends HttpServlet {
 					Connection conn = ConnectionFactory.getConnection();
 					System.out.println("Getting connection");
 					//Get connection
-					String sql = "SELECT * FROM EMPLOYEES WHERE EMPLOYEE_NAME=? and EMPLOYEE_PASSWORD=?";
+					String sql = "SELECT * FROM EMPLOYEES WHERE EMPLOYEE_NAME=? AND EMPLOYEE_PASSWORD=?";
 					//Execute statement
 					PreparedStatement stmt = conn.prepareStatement(sql);
 					//stmt = conn.prepareStatement(sql);
@@ -70,35 +66,56 @@ public class LoginServlet extends HttpServlet {
 					System.out.println(stmt.toString());
 					ResultSet rs = stmt.executeQuery();
 					// Calls next, always true if a next exists
-					if (rs.next()) 
-					{
-						if (rs.getString("EMPLOYEE_POSITION").equals("Employee")) 
-						{
+					if (rs.next()) {
+						if (rs.getString("EMPLOYEE_POSITION").equals("Employee")) {
 							RequestDispatcher rd = request.getRequestDispatcher("./Employee/mainEmployee.html");
 							rd.forward(request,response);
 						}
-						else 
-						{
+						else {
 							RequestDispatcher rd = request.getRequestDispatcher("index.html");
 							rd.forward(request,response);
 						}
 					}
-					else 
-					{
+					else {
 						RequestDispatcher rd = request.getRequestDispatcher("index.html");
 						rd.forward(request,response);
 					}
 					RequestDispatcher rd = request.getRequestDispatcher("index.html");
 					rd.forward(request,response);
-
-				}catch (SQLException e)
-				{
+				}catch (SQLException e)	{
 					e.printStackTrace();
 				}
 			}
 			else if (userType.equals("Manager")){
-				RequestDispatcher rd = request.getRequestDispatcher("./Manager/mainManager.html");
-				rd.include(request,response);
+				try {
+					Connection conn = ConnectionFactory.getConnection();
+					// Get connection
+					String sql = "SELECT * FROM EMPLOYEES WHERE EMPLOYEE_NAME=? AND EMPLOYEE_PASSWORD=?";
+					// Execute statement
+					PreparedStatement stmt = conn.prepareStatement(sql);
+					stmt.setString(1,  userName);
+					stmt.setString(2, password1);
+					ResultSet rs = stmt.executeQuery();
+					// Calls next, always true if a next exists
+					if (rs.next()) {
+						if (rs.getString("EMPLOYEE_POSITION").equals("Manager")) {
+							RequestDispatcher rd = request.getRequestDispatcher("./Manager/mainManager.html");
+							rd.forward(request,response);
+						}
+						else {
+							RequestDispatcher rd = request.getRequestDispatcher("index.html");
+							rd.forward(request,response);
+						}
+					}
+					else {
+						RequestDispatcher rd = request.getRequestDispatcher("index.html");
+						rd.forward(request,response);
+					}
+					RequestDispatcher rd = request.getRequestDispatcher("index.html");
+					rd.forward(request,response);
+				} catch (SQLException e)	{
+					e.printStackTrace();
+				}
 			}
 			else {
 				System.out.println("Error in userType check");
--- src/main/java/servlets/LogoutServlet.java
+++ src/main/java/servlets/LogoutServlet.java
@@ -1,10 +1,13 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LogoutServlet
@@ -23,15 +26,22 @@ public class LogoutServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+//		request.getRequestDispatcher("../Project1_1/index.html");
+		HttpSession session = request.getSession();
+		session.invalidate();
+		out.print("Logged Out Successfully");
+		out.close();
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
