aa1ccc91e27087d82087b52ad4e94ecca1187316
fachiolc <fachiolc@gmail.com>
2019-04-26 07:52:22 -0400

    Minimum Requirements Fulfulled - Manager can see all employees and all reimbursemets, Employee can add a reimbursement

--- src/main/java/daos/ReimbursementDaoImpl.java
+++ src/main/java/daos/ReimbursementDaoImpl.java
@@ -59,14 +59,24 @@ public class ReimbursementDaoImpl implements ReimbursementDao{
 	public Reimbursements addReimbursement(String reimbursementID, String requestAmount, String requesterID, String requestDescription, String status) {
 		
 		List<Reimbursements> reimbursements = new ArrayList<>();
-		
+//		
 		status = "pending";
 		try (Connection conn = ConnectionFactory.getConnection()) {
 			System.out.println("ReimbursementDaoImpl - Connected");
 			Statement stmt = conn.createStatement();
-			int rs = stmt.executeUpdate("INSERT INTO REIMBURSEMENTS (REIMBURSEMENT_ID,  REIMBURSEMENT_COST, REIMBURSEMENT_REQUESTER, REIMBURSEMENT_DESCRIPTION, REIMBURSEMENT_STATUS) VALUES('" +
-					reimbursementID + "','" + requestAmount + "','" + requesterID + "','" + requestDescription + "','" + "','pending')");
-			System.out.println("ReimbursementDaoImpl - Attempted to add reimbursement");
+			System.out.println("ReimbursementDaoImpl - Attempting to add reimbursement");
+			int rs = stmt.executeUpdate("INSERT INTO REIMBURSEMENTS "
+					+ "(REIMBURSEMENT_ID,  "
+					+ "REIMBURSEMENT_COST, "
+					+ "REIMBURSEMENT_REQUESTER, "
+					+ "REIMBURSEMENT_DESCRIPTION, "
+					+ "REIMBURSEMENT_STATUS) VALUES('" +
+					reimbursementID + "','" + 
+					requestAmount + "','" + 
+					requesterID + "','" + 
+					requestDescription + "','"
+							+ "pending')");
+			System.out.println("ReimbursementDaoImpl - Added reimbursement");
 			//			rs.next();
 			System.out.println(rs);
 		} catch (SQLException e) {
